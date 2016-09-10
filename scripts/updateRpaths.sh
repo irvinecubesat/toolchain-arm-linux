fb74969d36feaf6dc3273d3cf89932e839d58129
Maurice Ling <maurice@linglink.com>
2016-09-10 15:22:49 -0700

    set libexec rpaths, enhance updateRpath args to specify dir and rpath

--- scripts/updateRpaths.sh
+++ scripts/updateRpaths.sh
@@ -2,10 +2,42 @@
 #
 # update the Rpaths to use $ORIGIN/../lib instead of an absolute path
 #
-for file in *; do
+
+directory="."
+rpath="../lib"
+
+usage()
+{
+    cat <<EOF
+$0 [options]
+
+-p {rpath} relative path to set RPATH (default:  $rpath)
+-d {directory} Directory where the binaries are located (default: $directory)
+
+Updates the RPATH of the binaries with the given relative path
+EOF
+
+exit 1
+}
+
+while getopts "d:p:" arg; do
+    case $arg in
+	d)
+	    directory=$OPTARG
+	    ;;
+	p)
+	    rpath=$OPTARG
+	    ;;
+	*)
+	    usage
+	    ;;
+    esac
+done
+
+for file in $directory/*; do
   rpathVal=$(objdump -p $file |grep RPATH)
   if [ -n "$rpathVal" ]; then
       echo $file:  $rpathVal
-      chrpath -r "\$ORIGIN/../lib" $file
+      chrpath -r "\$ORIGIN/$rpath" $file
   fi
 done
