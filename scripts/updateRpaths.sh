5f7a8656c0721376f0d8902df11901d27d370007
Maurice Ling <maurice@linglink.com>
2016-11-20 12:43:02 -0800

    fix shellcheck warnings

--- scripts/updateRpaths.sh
+++ scripts/updateRpaths.sh
@@ -11,8 +11,10 @@ usage()
     cat <<EOF
 $0 [options]
 
--p {rpath} relative path to set RPATH (default:  $rpath)
+-p {rpath}     Relative path to set RPATH (default:  $rpath)
 -d {directory} Directory where the binaries are located (default: $directory)
+
+-h             This message
 
 Updates the RPATH of the binaries with the given relative path
 EOF
@@ -20,7 +22,7 @@ EOF
 exit 1
 }
 
-while getopts "d:p:" arg; do
+while getopts "d:p:h" arg; do
     case $arg in
 	d)
 	    directory=$OPTARG
@@ -28,16 +30,19 @@ while getopts "d:p:" arg; do
 	p)
 	    rpath=$OPTARG
 	    ;;
+	h)
+	    usage
+	;;
 	*)
 	    usage
 	    ;;
     esac
 done
 
-for file in $directory/*; do
-  rpathVal=$(objdump -p $file |grep RPATH)
+for file in "$directory"/*; do
+  rpathVal=$(objdump -p "$file" |grep RPATH)
   if [ -n "$rpathVal" ]; then
-      echo $file:  $rpathVal
-      chrpath -r "\$ORIGIN/$rpath" $file
+      echo "$file:  $rpathVal"
+      chrpath -r "\$ORIGIN/$rpath" "$file"
   fi
 done
