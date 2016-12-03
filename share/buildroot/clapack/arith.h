75b8185120b60a774f9e05a617394cb418e8bf1a
Maurice Ling <maurice@linglink.com>
2016-12-03 11:07:24 -0800

    Add arith.h for clapack compile

---
+++ share/buildroot/clapack/arith.h
@@ -0,0 +1,10 @@
+/*
+ * arith.h for clapack generated on arm5l Intrepid board
+ */
+#define IEEE_8087
+#define Arith_Kind_ASL 1
+#define Double_Align
+#define NO_LONG_LONG
+#define NANCHECK
+#define QNaN0 0x0
+#define QNaN1 0x7ff80000
